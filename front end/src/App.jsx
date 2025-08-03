39666aa4f83c81a512023999f9ff41b07fa0e15b
Abhal Benny <abhalbenny3@gmail.com>
2025-08-03 21:35:24 +0530

    rtgrthh

--- front end/src/App.jsx
+++ front end/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <Route path="/imp" element={<GenerateForm />} />
         {/* Add more routes as needed */}
          <Route path="/f" element={<FeeManagementDashboard/>} />
-        <Route path="/admission" element={<Admin/>} />       
+        <Route path="/admission" element={<Admission/>} />       
 
       </Routes>
     </Router>
